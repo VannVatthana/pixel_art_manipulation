980aa02ebe4dda0534aec5f00c42f38947bbac46
VannVatthana <vatthanavann@gmail.com>
2024-03-07 17:43:19 +0100

    correction

--- cgp/library/third_party/src/glad/opengl46/glad.d
+++
@@ -1,8 +0,0 @@
-../cgp/library/third_party/src/glad/opengl46/glad.o: \
- ../cgp/library/third_party/src/glad/opengl46/glad.cpp \
- ../cgp/library/third_party/src/glad/opengl46/glad.hpp \
- ../cgp/library/third_party/src/glad/opengl46/khrplatform.hpp \
- ../cgp/library/cgp/cgp_parameters.hpp
-../cgp/library/third_party/src/glad/opengl46/glad.hpp:
-../cgp/library/third_party/src/glad/opengl46/khrplatform.hpp:
-../cgp/library/cgp/cgp_parameters.hpp:
