1ee7305944331cbebd95e9486fe15c039ebc73d7
VannVatthana <vatthanavann@gmail.com>
2024-03-25 09:35:27 +0100

    correction

--- cgp/library/third_party/src/glad/opengl33/glad.d
+++
@@ -1,8 +0,0 @@
-../cgp/library/third_party/src/glad/opengl33/glad.o: \
- ../cgp/library/third_party/src/glad/opengl33/glad.cpp \
- ../cgp/library/third_party/src/glad/opengl33/glad.hpp \
- ../cgp/library/third_party/src/glad/opengl33/khrplatform.hpp \
- ../cgp/library/cgp/cgp_parameters.hpp
-../cgp/library/third_party/src/glad/opengl33/glad.hpp:
-../cgp/library/third_party/src/glad/opengl33/khrplatform.hpp:
-../cgp/library/cgp/cgp_parameters.hpp:
